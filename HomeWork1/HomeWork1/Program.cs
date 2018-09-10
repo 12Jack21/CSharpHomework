1d7f4ce706149589a9dbf9cb94573f1a4918ec4b
Johnson <32518669+12Jack21@users.noreply.github.com>
2018-09-10 21:20:25 +0800

    complete first week homework

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("============Calculate the product of two numbers==========");
+            Console.Out.WriteLine("Please enter two number below:");
+            double num1 = Double.Parse(Console.ReadLine());
+            double num2 = Double.Parse(Console.ReadLine());
+            Console.Out.WriteLine("The product of the two numbers is: " + num1 * num2);
+        }
+    }
+}
---
+++ HomeWork2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork2
+{
+    public partial class Form1 : Form
+    {
+        private double x, y; //保存用户输入的运算数
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Your input is null !!!", "Message tip", MessageBoxButtons.OK);
+            }
+            else
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                y = Convert.ToDouble(textBox2.Text);
+                textBox1.Text = "";
+                textBox2.Text = "";
+                label3.Text = (x * y).ToString();
+            }
+
+        }
+
+    }
+}
