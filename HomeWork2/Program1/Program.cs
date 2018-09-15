0206d7a61d2eea627375e83d492355c8bcdc97a3
Johnson <32518669+12Jack21@users.noreply.github.com>
2018-09-16 00:03:32 +0800

    second homework

---
+++ HomeWork2/Program1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tag = 0;
+            int num = Convert.ToInt32(Console.ReadLine());
+            int data = num;
+            int[] result = new int[100000];
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                while (true)
+                {
+                    if (num % i == 0)
+                    {
+                        result[tag] = i;
+                        tag++;
+                        num /= i;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            if (num != 1)
+            {
+                result[tag] = num;
+            }
+
+            Console.WriteLine("The Prime factor of " + data + " is:");
+            Console.Write(data + " = ");
+            for (int i = 0; i < tag + 1; i++)
+            {
+                if (i == tag)
+                {
+                    break;
+                }
+                if (i != 0)
+                {
+                    Console.Out.Write(" * ");
+
+                }
+                Console.Out.Write(result[i]);
+            }
+            Console.Out.WriteLine("");
+        }
+    }
+}
---
+++ HomeWork2/Program2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the length of the int array:");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the int value of the array:");
+            int[] data = new int[length];
+            for(int i = 0;i < length;i++)
+            {
+                data[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+    }
+}
