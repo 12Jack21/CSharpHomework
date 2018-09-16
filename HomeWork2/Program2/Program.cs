a6228ab6b5e6c802b903ab46ee96fa903f6b9f4c
Johnson <32518669+12Jack21@users.noreply.github.com>
2018-09-16 16:39:10 +0800

    second homework

--- HomeWork2/Program2/Program.cs
+++ HomeWork2/Program2/Program.cs
@@ -10,6 +10,7 @@ namespace Program2
     {
         static void Main(string[] args)
         {
+            //从屏幕读取数据
             Console.WriteLine("Please enter the length of the int array:");
             int length = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the int value of the array:");
@@ -19,6 +20,39 @@ namespace Program2
                 data[i] = Convert.ToInt32(Console.ReadLine());
             }
 
+            //寻找最小值
+            int min = data[0];
+            for(int i = 1;i <length;i++)
+            {
+                if(min >= data[i])
+                {
+                    min = data[i];
+                }
+            }
+            Console.WriteLine("The miximum of the int array is : " + min);
+
+            //寻找最大值
+            int max = data[0];
+            for (int i = 1; i < length; i++)
+            {
+                if (max <= data[i])
+                {
+                    max = data[i];
+                }
+            }
+            Console.WriteLine("The maximum of the int array is : " + max);
+
+            //求和
+            int sum = 0;
+            for(int i = 0;i < length;i++)
+            {
+                sum += data[i];
+            }
+            Console.WriteLine("The sum of the int array is: " + sum);
+
+            //求平均值
+            double average = sum / (length * 1.0);
+            Console.WriteLine("The average of the int array is: " + average);
         }
     }
 }
--- HomeWork2/Program3/Program.cs
+++ HomeWork2/Program3/Program.cs
@@ -10,6 +10,42 @@ namespace Program3
     {
         static void Main(string[] args)
         {
+            int tag = 0;
+            int[] prime = new int[100];
+            //筛选素数
+            for (int i = 2; i <= 100; i++)
+            {
+                if (i % 2 == 0 && i != 2)
+                {
+                    continue;
+                }
+                else if (i % 3 == 0 && i != 3)
+                {
+                    continue;
+                }
+                else if (i % 5 == 0 && i != 5)
+                {
+                    continue;
+                }
+                else if (i % 7 == 0 && i != 7)
+                {
+                    continue;
+                }
+                else
+                {
+                    prime[tag] = i;
+                    tag++;
+                }
+            }
+
+            //输出
+            Console.WriteLine("The prime number from 2 to 100 are: ");
+            for (int i = 0; i < tag; i++)
+            {
+                Console.Write(prime[i] + " ");
+
+            }
+            Console.WriteLine("");
         }
     }
 }
