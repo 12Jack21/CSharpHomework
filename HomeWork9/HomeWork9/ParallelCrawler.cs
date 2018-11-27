f89f59ec58b8c0f921aceedd91100f2db9b3abf0
Johnson <32518669+12Jack21@users.noreply.github.com>
2018-11-27 21:47:22 +0800

    Nineth homework

---
+++ HomeWork9/HomeWork9/ParallelCrawler.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+//将书中例9-10的爬虫程序，使用并行处理进行优化，实现更快速的网页爬取。
+namespace HomeWork9
+{
+    class ParallelCrawler
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        private int count = 0;
+        private int max = 100;
+        public ConcurrentBag<String> complated = new ConcurrentBag<string>();
+        public ConcurrentQueue<String> pending = new ConcurrentQueue<string>();
+
+        public void Start(String url, int max)
+        {
+            stopWatch.Start();
+            this.count = 0;
+            this.max = max;
+
+            Task.Run(() => DownloadAndParse(url));
+            while (count < max)
+            {
+                String ur;
+                pending.TryDequeue(out ur);
+                if (ur != null)
+                    Task.Run(() => DownloadAndParse(ur));
+            }
+            stopWatch.Stop();
+            Console.WriteLine("time cost:" + stopWatch.ElapsedMilliseconds);
+        }
+
+        public void DownloadAndParse(String url)
+        {
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            try
+            {
+                Console.WriteLine($"Downlonding {url}");
+                String page = client.DownloadString(url);
+                File.WriteAllText(count.ToString(), page, Encoding.UTF8);
+                count++;
+                Console.WriteLine($"Parsing {url}");
+                Parse(page);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void Parse(string page)
+        {
+
+            HashSet<String> urls = new HashSet<string>();
+            string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+
+            MatchCollection matches = new Regex(strRef).Matches(page);
+            foreach (Match match in matches)
+            {
+                String url = match.Value.Substring(match.Value.IndexOf("=") + 1)
+                  .Trim('"', '\"', '#', ',', '>');
+                if (url.Length == 0) continue;
+                if (!pending.Contains(url) || !complated.Contains(url))
+                    pending.Enqueue(url);
+            }
+        }
+
+
+    }
+}
+
---
+++ HomeWork9/HomeWork9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//将书中例9-10的爬虫程序，使用并行处理进行优化，实现更快速的网页爬取。
+namespace HomeWork9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String startUrl = "http://www.cnblogs.com/dstang2000/";
+            Crawler crawler1 = new Crawler();
+            ParallelCrawler crawler2 = new ParallelCrawler();
+            //时间对比
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            crawler1.Start(startUrl, 20);
+            sw.Stop();
+            Console.WriteLine("Normal crawler crawling time: " + sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            crawler2.Start(startUrl,20);
+            sw.Stop();
+            Console.WriteLine("Parallel crawler crawling time: " + sw.ElapsedMilliseconds);
+
+            Console.ReadKey();
+        }
+    }
+}
